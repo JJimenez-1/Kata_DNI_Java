98aaf5f1fda38b2405a802f67dc3648607936004
José <jjimenez@cifpfbmoll.eu>
2020-02-19 16:27:21 +0100

    feat(dni): Añadir caso test para el constructor de la clase dni.

--- src/test/java/edu/elsmancs/dni/dniTest.java
+++ src/test/java/edu/elsmancs/dni/dniTest.java
@@ -7,6 +7,13 @@ import static org.junit.Assert.*;
 public class dniTest {
 
     @Test
+    public void constructorTest() {
+        dni dni = new dni("01817200Q");
+        assertEquals("01817200Q", dni.toString());
+    }
+
+    @Test
+
     public void getDniTest() {
         dni dni = new dni("01817200Q");
         assertEquals("01817200Q", dni.getDni());
