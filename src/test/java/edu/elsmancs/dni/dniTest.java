da47a09fa6e2f2df81f150ab7fac8597bde3746c
José <jjimenez@cifpfbmoll.eu>
2020-02-19 16:31:38 +0100

    feat(dni): Test para comprobar longitud del DNI

--- src/test/java/edu/elsmancs/dni/dniTest.java
+++ src/test/java/edu/elsmancs/dni/dniTest.java
@@ -13,10 +13,15 @@ public class dniTest {
     }
 
     @Test
-
     public void getDniTest() {
         dni dni = new dni("01817200Q");
         assertEquals("01817200Q", dni.getDni());
+    }
+
+    @Test
+    public void longDniTest() {
+        dni dni = new dni("01817200Q");
+        assertTrue(dni.checkDniLength());
     }
     /**
     @Test
